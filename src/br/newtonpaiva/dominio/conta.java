ec1685333b781d5e48ae0348b0d35c7fe9ea39ff
Gabriel da Costa Rodrigues <112523344+GABRP@users.noreply.github.com>
2023-03-18 17:55:59 -0300

    feat: implementa construtores, equals, hashcode e transferir

--- src/br/newtonpaiva/dominio/conta.java
+++ src/br/newtonpaiva/dominio/conta.java
@@ -1,18 +1,45 @@
 package br.newtonpaiva.dominio;
+
+import java.util.Objects;
+
 public class conta  {
     private Integer numero;
+
     private Double saldo;
 
+    //construtor
+    public conta(){
+        this(null);
+    }
+    //construtor
+    public conta(Integer numero){
+        this(numero, 0.0);
+    }
+    //construtor
+    public conta(Integer numero, Double saldo){
+        this.numero = numero;
+        this.saldo = saldo;
+    }
     public Double sacar(Double valor){
 
         return 0.0;
     }
     public Double depositar(Double valor){
-
         if(valor == null || valor <=0)
             throw new IllegalArgumentException("depósito mínimo de 1 centavo");
         saldo += valor;
         return saldo;
+    }
+    public void transferir(conta destino, Double valor){
+        if(this.getSaldo() < valor)
+            throw new IllegalArgumentException( "Invalido");
+
+        if(this.equals(destino))
+            throw new IllegalArgumentException("Invalido");
+        this.sacar(valor);
+        destino.depositar(valor);
+
+
     }
     public Integer getNumero(){
 
@@ -21,7 +48,6 @@ public class conta  {
     public void setNumero(Integer numero){
         if(numero == null || numero < 0)
             throw new IllegalArgumentException(" Erro, número inválido");
-
         this.numero = numero;
     }
     public void setSaldo(Double saldo){
@@ -30,10 +56,21 @@ public class conta  {
     }
     public Double getSaldo(){
 
-        return saldo;
+            return saldo;
 
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            conta conta = (conta) o;
+            return Objects.equals(numero, conta.numero);
+        }
+
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(numero);
+        }
     }
-
-
-
-}
--- src/br/newtonpaiva/ui/Main.java
+++ src/br/newtonpaiva/ui/Main.java
@@ -4,18 +4,25 @@ import br.newtonpaiva.dominio.conta;
 
 public class Main {
     public static void main(String[] args) {
-        conta c = new conta();
-        c.setNumero(10);
+        conta c = new conta(10);
+//        c.setNumero(10);
         c.setSaldo(100.0);
 
+        c.depositar( 50.0);
+//        c.depositar( 50.0);
+
         conta c2 = new conta();
-        c2.setNumero(9);
-        c2.setSaldo(60.0);
+        c2.setNumero (10);
+        c2.setSaldo(500.0);
 
+        conta c3 = new conta();
 
-        c.depositar( 50.0);
-        c.depositar( 50.0);
-
+        if(c.equals(c2)){
+            System.out.println("Contas iguais");
+        }
+        else{
+            System.out.println("Contas divergentes");
+        }
         System.out.println("você possui " +c.getSaldo()+ " reais de saldo em sua conta");
     }
 }
